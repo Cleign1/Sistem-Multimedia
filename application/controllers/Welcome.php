6ff67b49e3b37ca9a9d246ad57a34be08ffcc910
Cleign1 <ibnukhaidar@gmail.com>
2025-04-10 15:14:51 +0700

    Refactor Welcome controller to improve structure and enhance view loading

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -2,24 +2,49 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Welcome extends CI_Controller {
-
-	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see https://codeigniter.com/user_guide/general/urls.html
-	 */
+	 public function __construct() {
+		parent::__construct();
+		$this->load->model('M_welcome', 'model');
+		$this->load->helpers('url');
+		$this->load->library('session');
+	 }
 	public function index()
 	{
-		$this->load->view('welcome_message');
+		$this->load->view('header');
+		$this->load->view('home');
+		$this->load->view('footer');
+	}
+
+	public function create() {
+		$this->load->helper('form');
+		$this->load->library('form_validation');
+
+		$this->form_validation->set_rules('name', 'Name', 'required');
+		$this->form_validation->set_rules('description', 'Description','required');
+
+		if( $this->form_validation->run() == FALSE ) {
+			$this->load->view('header');
+			$this->load->view('create');
+			$this->load->view('footer');
+		} else {
+			$id = uniqid('item', true);
+
+			$config['upload_path'] = './upload/post';
+			$config['allowed_types'] = 'jpg|png|jpeg';
+			$config['max_size'] = '100000';
+			$config['file_ext_tolower'] = TRUE;
+			$config['filename'] = str_replace('.', '_', $id);
+
+			$this->load->library('upload', $config);
+
+			if( $this->upload->do_upload('image1') == FALSE ) {
+				$this->session->set_flashdata('error', $this->upload->display_errors());
+				redirect('welcome/index');
+			} else {
+				$filename = $this->upload->data('file_name');
+				$this->model->create($id, $filename);
+				redirect('');
+			}
+		}
 	}
 }
